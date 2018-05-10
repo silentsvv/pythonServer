2dc21386bc98ac52aeda9be69764e9d49ffc7458
root <root@vultr.guest>
2018-05-10 16:14:12 +0000

    add: 添加sh脚本

--- app/__init__.py
+++ app/__init__.py
@@ -1,31 +1,34 @@
 from flask import Flask
-from flask import request
+import time
+from flask import Flask, request, make_response
 import hashlib
+import json
+import xml.etree.ElementTree as ET
+
+
 app = Flask(__name__)
+app.debug = True
 
-@app.route('/weixin',methods=['GET','POST'])
+
+@app.route('/')  # 默认网址
 def index():
-    if request.method == "GET":
-        my_signature = request.args.get('signature')     # 获取携带的signature参数
-        my_timestamp = request.args.get('timestamp')     # 获取携带的timestamp参数
-        my_nonce = request.args.get('nonce')        # 获取携带的nonce参数
-        my_echostr = request.args.get('echostr')         # 获取携带的echostr参数
+    return 'Index Page'
 
-        token = 'hahaha'     # 一定要跟刚刚填写的token一致
 
-        # 进行字典排序
-        data = [token,my_timestamp ,my_nonce ]
-        data.sort()
-        
-        # 拼接成字符串
-        temp = ''.join(data)
+@app.route('/wx', methods=['GET', 'POST'])
+def wechat_auth():  # 处理微信请求的处理函数，get方法用于认证，post方法取得微信转发的数据
+    if request.method == 'GET':
+        token = '你自己设置好的token'
+        data = request.args
+        signature = data.get('signature', '')
+        timestamp = data.get('timestamp', '')
+        nonce = data.get('nonce', '')
+        echostr = data.get('echostr', '')
+        s = [timestamp, nonce, token]
+        s.sort()
+        s = ''.join(s)
+        if (hashlib.sha1(s).hexdigest() == signature):
+            return make_response(echostr)
 
-        # 进行sha1加密
-        mysignature = hashlib.sha1(temp).hexdigest()
-        
-        # 加密后的字符串可与signature对比，标识该请求来源于微信
-        if my_signature == mysignature:
-            return my_echostr
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000)
---
+++ run.sh
@@ -0,0 +1,5 @@
+base='/home/wwwroot/pythonServer'
+echo $base
+source $base/env/bin/activate
+python3 $base/app/__init__.py
+
